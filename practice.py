afc474fc8d5b01a77fb04d2f9b3edc0470623c35
Tweakers <tweatershin@gmail.com>
2019-08-10 19:36:53 +0900

    ml

---
+++ practice.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Author: 'Tweakers'
+#         'tweatershin@gmail.com'
+#         
+# Created on 17/07/2019
+
+import tensorflow as tf
+
+tf.gradients()
